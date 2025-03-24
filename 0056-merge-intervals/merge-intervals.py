169aaa3ffde4db6305f5ce895b436b442b8e630b
RuhamaGetahun <rmg32@calvin.edu>
2025-03-24 00:46:56 -0400

    Time: 6 ms (78.53%) Space: 21.7 MB (14.66%)

---
+++ 0056-merge-intervals/merge-intervals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # create a new list to add on the non-overlapping intervals 
+        output = []
+        [[0,2], [1,4], [3,5]]
+
+        # sort the array 
+        intervals.sort(key = lambda x:x[0])
+        # hold on to an end 
+        output.append(intervals[0])
+        # compare by going from 1 and above 
+        for start, end in intervals[1:]:
+            last_end = output[-1][1]
+            if start <= last_end:
+                output[-1][1] = max(last_end, end)
+            else:
+                output.append([start, end])
+        return output 
+
+        
