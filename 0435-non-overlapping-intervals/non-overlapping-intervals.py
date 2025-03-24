cc6c1a5ba457bbd022ffd0d3c27e7a5330a07d75
RuhamaGetahun <rmg32@calvin.edu>
2025-03-24 00:17:16 -0400

    Time: 89 ms (57.01%) Space: 51.4 MB (54.68%)

--- 0435-non-overlapping-intervals/non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -7,7 +7,7 @@ class Solution:
         # end needs to be the last non overlapping end 
 
         # [[1,2],[1,3],[2,3],[3,4]]
-        #[[1,100],[1,11],[2,12],[11,22]]
+        #[[1, 11], [2, 12], [11, 22], [1, 100]]
         end = intervals[0][1]
         for i in range(1, len(intervals)): 
             if intervals[i][0] < end:
