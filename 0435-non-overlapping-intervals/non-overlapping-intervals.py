ed426d0e23b2ac0d3a5c84d10e2cc5236ee0c61d
RuhamaGetahun <rmg32@calvin.edu>
2025-03-24 00:02:38 -0400

    Time: 91 ms (55.29%) Space: 51.2 MB (72.07%)

---
+++ 0435-non-overlapping-intervals/non-overlapping-intervals.py
@@ -0,0 +1,19 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        no_deleted_list = 0
+        # sort the intervals 
+        intervals.sort(key=lambda x:x[1])
+
+        # end needs to be the last non overlapping end 
+
+        # [[1,2],[1,3],[2,3],[3,4]]
+        #[[1,100],[1,11],[2,12],[11,22]]
+        end = intervals[0][1]
+        for i in range(1, len(intervals)): 
+            if intervals[i][0] < end:
+                no_deleted_list += 1
+            else:
+                end = intervals[i][1]
+               
+
+        return no_deleted_list
