243125f3998e9fd952272d355e273f56a59f0b69
RuhamaGetahun <rmg32@calvin.edu>
2025-07-01 07:05:23 -0600

    Time: 7 ms (59.95%) Space: 18.4 MB (49.16%)

--- 0032-longest-valid-parentheses/longest-valid-parentheses.py
+++ 0032-longest-valid-parentheses/longest-valid-parentheses.py
@@ -1,15 +1,23 @@
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
         stack = [-1]
-        length = 0  
+        max_length = 0
 
         for i, char in enumerate(s):
-            if char == "(":
+            if s[i] == '(':
                 stack.append(i)
             else:
                 stack.pop()
                 if not stack:
                     stack.append(i)
                 else:
-                    length = max(length, i - stack[-1])
-        return length 
+                    max_length = max(max_length, i - stack[-1])
+        
+        return max_length 
+
+        #I need a index that would hold on to the closest opening parenthesis 
+        # and a stack to help me work with adding them 
+        # if i end up getting an opening braket i would add it to the stack and update my index 
+        # if stack and if stack[-1] is ( and the s[i] is ) substarct the i and the opening paren index 
+        # 
+ 
