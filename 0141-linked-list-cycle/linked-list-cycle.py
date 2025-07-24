c65a8ff3ccad044272fcb85c37013e7f9c2c315b
RuhamaGetahun <rmg32@calvin.edu>
2025-07-24 11:37:28 -0400

    Time: 50 ms (46.06%) Space: 19.8 MB (56.96%)

--- 0141-linked-list-cycle/linked-list-cycle.py
+++ 0141-linked-list-cycle/linked-list-cycle.py
@@ -8,11 +8,13 @@ class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         fast = head 
         slow = head 
+
         while fast and fast.next:
             slow = slow.next
-            fast = fast.next.next 
+            fast = fast.next.next
             if slow == fast:
-                return True  
-        return False  
+                return True 
+        return False 
+
 
         
