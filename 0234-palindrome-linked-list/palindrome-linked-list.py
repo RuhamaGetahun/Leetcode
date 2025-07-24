5ce8736cd12d2cc0ea29e2da094af79e1da6a222
RuhamaGetahun <rmg32@calvin.edu>
2025-07-24 11:52:54 -0400

    Time: 51 ms (17.28%) Space: 34.6 MB (95.82%)

--- 0234-palindrome-linked-list/palindrome-linked-list.py
+++ 0234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,33 +5,31 @@ class ListNode:
         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        #checks out if the linked list doesn't have anything or just one 
+        #checks out if the linked list doesn't have anything or just one
         if not head or not head.next:
             return True 
 
-        # find the middle node 
+         # find the middle node
         fast = head 
         slow = head 
         while fast and fast.next:
             slow = slow.next 
-            fast = fast.next.next 
-        
-        # reverse the linked list 
+            fast = fast.next.next
+
+        # reverse the last half of the linked list 
         prev = None 
-        current = slow
+        current = slow 
         while current:
             next_ = current.next 
             current.next = prev 
             prev = current 
-            current = next_
+            current = next_ 
 
-        # now compare them 
         first = head 
-        last = prev 
-
-        while last:# only needs to check half 
-            if first.val != last.val:
+        tail = prev 
+        while tail: # only needs to check half
+            if first.val != tail.val:
                 return False 
-            first = first.next
-            last = last.next 
-        return True 
+            first= first.next 
+            tail = tail.next 
+        return True  
