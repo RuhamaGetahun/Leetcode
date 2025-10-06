9ea9f22c5d4d460ff4dba861aaf7ccc2659f0b23
RuhamaGetahun <rmg32@calvin.edu>
2025-10-06 14:23:55 -0400

    Time: 3 ms (49.89%) Space: 18.8 MB (62.38%)

--- 0001-two-sum/two-sum.py
+++ 0001-two-sum/two-sum.py
@@ -1,11 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #hash-maps 
-        l, r = 0, len(nums) - 1
-        res = {}
+        l, r = 0, len(nums) - 1 
+        seen = {} # hash 
 
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in res:
-                return [res[complement], i]
-            res[num] = i  # store num we have already seen.. 
+        for i , num in enumerate(nums): # iterate 
+            complement = target - num 
+
+            if complement in seen:
+                return ([seen[complement], i])
+
+            seen[num] = i 
+            
